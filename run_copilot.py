db3136819c13ba95c49b94bfcc37428fefac9c4e
WH-Yoshi <absluca419@gmail.com>
2024-05-08 10:34:37 +0200

    22/04/2024 - Polarion and Copilot ready for prod

--- run_copilot.py
+++ run_copilot.py
@@ -1,12 +1,15 @@
+import os
 import platform
 import subprocess
+from pathlib import Path
 
 
 def run_copilot():
+    print(Path(r'.\codes\launchers\Launcher_copilot.cmd').absolute())
     if platform.system() == 'Windows':
-        subprocess.call(['start', './codes/launchers/Launcher_copilot.cmd'])
+        os.system('cmd /c "start /wait ' + str(Path(r'.\codes\launchers\Launcher_copilot.cmd').absolute()) + '"')
     elif platform.system() == 'Linux':
-        subprocess.call(['sh', './codes/launchers/Launcher_copilot.sh'])
+        os.system('sh ' + str(Path(r'.\codes/launchers/Launcher_copilot.sh').absolute()))
     else:
         raise OSError('Unsupported OS')
 
