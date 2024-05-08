fbb2432c2678bcdbdf7b7dc0f244c23566699956
WH-Yoshi <absluca419@gmail.com>
2024-05-08 10:37:27 +0200

    22/04/2024 - Polarion and Copilot ready for prod

--- run_copilot.py
+++ run_copilot.py
@@ -9,7 +9,7 @@ def run_copilot():
     if platform.system() == 'Windows':
         os.system('cmd /c "start /wait ' + str(Path(r'.\codes\launchers\Launcher_copilot.cmd').absolute()) + '"')
     elif platform.system() == 'Linux':
-        os.system('sh ' + str(Path(r'.\codes/launchers/Launcher_copilot.sh').absolute()))
+        os.system('sh ' + str(Path(r'codes/launchers/Launcher_copilot.sh').absolute()))
     else:
         raise OSError('Unsupported OS')
 
