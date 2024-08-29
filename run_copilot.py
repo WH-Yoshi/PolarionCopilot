47e80748502626f8d096b9b2a33b2e516dc3d938
Luca <AIXYF@goiba.net>
2024-08-29 10:30:06 +0200

    Certs to test on linux and windows

--- run_copilot.py
+++ run_copilot.py
@@ -1,11 +1,14 @@
 import os
 import platform
+import subprocess
 from pathlib import Path
 
 
 def run_copilot():
     if platform.system() == 'Windows':
         os.system('cmd /c "start /wait ' + str(Path(r'.\codes\launchers\Launcher_copilot.cmd').absolute()) + '"')
+    elif platform.system() == 'Linux':
+        subprocess.run(['bash', str(Path(r'.\codes\launchers\Launcher_copilot.sh').absolute())], check=True)
     else:
         raise OSError('Unsupported OS yet')
 
