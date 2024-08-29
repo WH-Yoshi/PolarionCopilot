2e18d0b16025318ce1c3c1eb32dae85b3d48d517
Luca <AIXYF@goiba.net>
2024-08-29 11:03:42 +0200

    Certs to test on linux and windows

--- run_polarion.py
+++ run_polarion.py
@@ -8,7 +8,7 @@ def run_copilot():
     if platform.system() == 'Windows':
         os.system('cmd /c "start /wait ' + str(Path(r'.\codes\launchers\Launcher_polarion.cmd').absolute()) + '"')
     elif platform.system() == 'Linux':
-        subprocess.run(['bash', str(Path(r'.\codes\launchers\Launcher_polarion.sh').absolute())], check=True)
+        subprocess.run(['bash', str(Path(r'./codes/launchers/Launcher_polarion.sh').absolute())], check=True)
     else:
         raise OSError('Unsupported OS yet')
 
