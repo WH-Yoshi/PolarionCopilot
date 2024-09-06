e1bf2725a3876424f27e420ede8c775ddbc84b8c
Arnaud Van Eenoo <arnaud.vaneenoo@iba-group.com>
2024-09-06 11:02:00 +0200

    Fixing Launcher_copilot.ps1 again

--- codes/launchers/Launcher_copilot.ps1
+++ codes/launchers/Launcher_copilot.ps1
@@ -4,8 +4,8 @@ $mistral_port = 22028
 $embedding_remote_port = "22016"
 $mistral_remote_port = "22002"
 
-$ssh_embedding = "ssh -N -f -p $embedding_remote_port user@northcarolina-b.tensordockmarketplace.com -i ~\.ssh\id_rsa_tensordock -L 22027:localhost:8080"
-$ssh_mistral = "ssh -N -f -p $mistral_remote_port user@idaho-b.tensordockmarketplace.com -i ~\.ssh\id_rsa_tensordock -L 22028:localhost:8000"
+$ssh_embedding = "ssh -N -f -p $embedding_remote_port user@northcarolina-b.tensordockmarketplace.com -i ~\.ssh\id_rsa_tensordock -L 22027:localhost:8080 2>`$null"
+$ssh_mistral = "ssh -N -f -p $mistral_remote_port user@idaho-b.tensordockmarketplace.com -i ~\.ssh\id_rsa_tensordock -L 22028:localhost:8000 2>`$null"
 
 function exec_ssh($SSH_COMMAND) {
     Start-Process -FilePath "powershell" -ArgumentList "-Command", $SSH_COMMAND -NoNewWindow
@@ -31,17 +31,17 @@ if (-not $mistral_PORT_STATUS) {
 }
 
 # Verify remote connections
-$embedding_STATUS = Get-NetTCPConnection -State Established -RemotePort $embedding_remote_port
-$mistral_STATUS = Get-NetTCPConnection -State Established -RemotePort $mistral_remote_port
+$embedding_STATUS = Get-NetTCPConnection -State Established -RemotePort $embedding_remote_port 2>`$null
+$mistral_STATUS = Get-NetTCPConnection -State Established -RemotePort $mistral_remote_port 2>`$null
 
 if ($embedding_STATUS -and $mistral_STATUS) {
     message($embedding_port)
     message($mistral_port)
-    Write-Host "`n"
     python .\codes\before_code.py
     python .\codes\Polarion.py
 } else {
-    Write-Host "Please check the Remote Virtual Machine. They must be running to use the APP!" -ForegroundColor Red
+    Write-Host "Execution of one or both SSH tunnel was unsuccessful." -ForegroundColor Red
+    Write-Host "`nPlease check the Remote Virtual Machine. They must be running to use the APP!" -ForegroundColor Yellow
     Write-Host "Follow these instruction for the deployment of the cloud GPU via TensorDock." -ForegroundColor Yellow
-    Write-Host "https://gitlab.sw.goiba.net/req-test-tools/polarion-copilot/copilot-proto#polarioncopilot`n" -ForegroundColor Yellow
+    Write-Host "https://gitlab.sw.goiba.net/req-test-tools/polarion-copilot/copilot-proto#polarioncopilot`n" -ForegroundColor Blue
 }
