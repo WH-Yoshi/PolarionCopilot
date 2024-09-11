526fa81f3e030bae1371c353c21d5d9664771938
Luca <AIXYF@goiba.net>
2024-09-11 15:24:51 +0200

    Screen changes for better automation

--- codes/launchers/Launcher_polarion.sh
+++ codes/launchers/Launcher_polarion.sh
@@ -6,15 +6,15 @@ PORT=22027
 SSH_COMMAND="ssh -N -f -p 22016 user@northcarolina-b.tensordockmarketplace.com -i ~/.ssh/id_rsa_tensordock -L $PORT:localhost:8080"
 
 if nc -zv localhost "$PORT" 2>&1 | grep -q 'succeeded'; then
-  echo "\e[32Port to Embedding machine: $PORT is open on localhost\e[0m"
+  echo "\e[32mPort to Embedding machine: $PORT is open on localhost\e[0m"
 else
-  echo "\e[33Port to Embedding machine: $PORT is closed on localhost. Opening SSH tunnel...\e[0m"
+  echo "\e[33mPort to Embedding machine: $PORT is closed on localhost. Opening SSH tunnel...\e[0m"
   $SSH_COMMAND
   if [ $? -eq 0 ]; then
-    echo "\e[32SSH command successful.\e[0m"
+    echo "\e[32mSSH command successful.\e[0m"
   else
     echo "Error: SSH command failed with exit code $?."
-    echo "\e[33Remote machine is down, workitems will be saved locally for later.\e[0m"
+    echo "\e[33mRemote machine is down, workitems will be saved locally for later.\e[0m"
   fi
 fi
 
